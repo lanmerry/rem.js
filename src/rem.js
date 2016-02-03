5a5b98e7c5ff120752535da4d13611a56b662999
banricho <i@banri.me>
2016-02-04 02:32:08 +0800

    fix typo

--- src/rem.js
+++ src/rem.js
@@ -3,14 +3,11 @@
         let rootEl = win.document.documentElement,
             cliSize = rootEl.getBoundingClientRect(),
             width = cliSize.width,
-            height = cliSize.height,
-            rem;
+            height = cliSize.height;
 
-        if ((width / height) > (750 / 1334)) {
-            rem = height / (1334 / 50);
-        } else {
-            rem = width / (750 / 50);
-        }
+        let rem = (width / height) > (750 / 1334) ?
+            height / (1334 / 50) :
+            width / (750 / 50);
 
         rootEl.style.fontSize = rem + 'px';
     };
